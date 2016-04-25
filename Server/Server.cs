234dafa26480c21bf06f2a2c443ebd0aa5accb07
rgawry <rgawry@users.noreply.github.com>
2016-04-25 16:33:18 +0200

    added IServer

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,57 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class Server
+    public interface IServer : IDisposable
+    {
+        void Connect();
+        void Bind(string name);
+    }
+
+    public class Server : IServer
+    {
+        private ConnectionFactory _connectionFactory = new ConnectionFactory() { HostName = "10.48.13.111", Port = 5672 };
+        private IConnection _connection;
+        private IModel _channel;
+        private IList<string> _declaredQueues;
+
+        public void Bind(string name)
+        {
+            string queueName = string.Empty;
+            if (!_declaredQueues.Contains(name))
+            {
+                queueName = _channel.QueueDeclare().QueueName;
+                _declaredQueues.Add(queueName);
+            }
+            _channel.QueueBind(queue: queueName, exchange: "", routingKey: name);
+        }
+
+        public void Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_channel != null) _channel.Dispose();
+            if (_connection != null) _channel.Dispose();
+        }
+    }
+
+    class ServerApp
     {
         static void Main(string[] args)
         {
-
+            var factory = new ConnectionFactory() { HostName = "10.48.13.111", Port = 5672 };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "communicator-main",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+            }
         }
     }
 }
