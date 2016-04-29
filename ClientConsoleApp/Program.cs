8d7b16f609ebaf7300ba43b1d9d4e7031b2d755e
rgawry <rgawry@users.noreply.github.com>
2016-04-29 15:57:05 +0200

    removed Client, Server projects, RabbitMqConsume

--- ClientConsoleApp/Program.cs
+++ ClientConsoleApp/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Client;
-using Common;
 
 namespace ClientConsoleApp
 {
@@ -12,7 +10,7 @@ namespace ClientConsoleApp
     {
         static void Main(string[] args)
         {
-            
+           
         }
     }
 }
