0bd52a24203ab44b2b776b8e8846060c3795b8cb
rgawry <rgawry@users.noreply.github.com>
2016-04-29 12:21:52 +0200

    added login verification in server

--- Common/IBus.cs
+++ Common/IBus.cs
@@ -22,52 +22,51 @@ namespace Common
         private ConnectionFactory _factory = new ConnectionFactory() { HostName = "10.48.13.111", Port = 5672 };
 
         IConnection connection;
-        IModel channel;
-        IModel channel2;
+        IModel channelConsume;
+        IModel channelProduce;
 
         public RabbitMqBus(string exchangeName, string requestQueueName)
         {
             _exchangeName = exchangeName;
             _requestQueueName = requestQueueName;
             connection = _factory.CreateConnection();
-            channel = connection.CreateModel();
-            channel2 = connection.CreateModel();
+            channelConsume = connection.CreateModel();
+            channelProduce = connection.CreateModel();
         }
-
-        public Task<TResult> Request<TRequest, TResult>(TRequest request)
+        
+        public Task<TResponse> Request<TRequest, TResponse>(TRequest request)
         {
-            TaskCompletionSource<TResult> result = new TaskCompletionSource<TResult>();
+            TaskCompletionSource<TResponse> result = new TaskCompletionSource<TResponse>();
             var responseQueueName = string.Empty;
 
-            channel.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
-            responseQueueName = channel.QueueDeclare().QueueName;
-            channel.QueueBind(responseQueueName, _exchangeName, responseQueueName);
-            var consumer = new EventingBasicConsumer(channel);
+            channelConsume.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
+            responseQueueName = channelConsume.QueueDeclare().QueueName;
+            channelConsume.QueueBind(responseQueueName, _exchangeName, responseQueueName);
+            var consumer = new EventingBasicConsumer(channelConsume);
             consumer.Received += (sender, args) =>
             {
                 var bodyJson = Encoding.UTF8.GetString(args.Body);
-                var responseMessage = JsonConvert.DeserializeObject<TResult>(bodyJson);
+                var responseMessage = JsonConvert.DeserializeObject<TResponse>(bodyJson);
                 result.SetResult(responseMessage);
             };
-            channel.BasicConsume(responseQueueName, true, consumer);
+            channelConsume.BasicConsume(responseQueueName, true, consumer);
 
-            channel2.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
-            channel2.QueueDeclare(_requestQueueName, false, false, false, null);
-            channel2.QueueBind(_requestQueueName, _exchangeName, _requestQueueName);
-
-            var basicProperties = channel2.CreateBasicProperties();
+            channelProduce.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
+            channelProduce.QueueDeclare(_requestQueueName, false, false, false, null);
+            channelProduce.QueueBind(_requestQueueName, _exchangeName, _requestQueueName);
+            var basicProperties = channelProduce.CreateBasicProperties();
             basicProperties.Headers = new Dictionary<string, object>();
             basicProperties.Headers.Add(_responseQueueHeaderKey, responseQueueName);
             var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(request));
-            channel2.BasicPublish(_exchangeName, _requestQueueName, basicProperties, body);
+            channelProduce.BasicPublish(_exchangeName, _requestQueueName, basicProperties, body);
 
             return result.Task;
         }
 
         public void Dispose()
         {
-            channel.Dispose();
-            channel2.Dispose();
+            channelConsume.Dispose();
+            channelProduce.Dispose();
             connection.Dispose();
         }
     }
--- Server/Server.cs
+++ Server/Server.cs
@@ -17,9 +17,8 @@ namespace Server
 
     public class Server : IServer
     {
-        private List<string> _users;
+        private List<string> _users = new List<string>();
         private string _exchangeName = "session-exchange";
-
         ConnectionFactory factory = new ConnectionFactory() { HostName = "10.48.13.111", Port = 5672 };
         IConnection connection;
         IModel channel;
@@ -32,7 +31,6 @@ namespace Server
 
         public void ListenForNewSession()
         {
-
             channel.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
             channel.QueueDeclare("session-request", false, false, false, null);
             channel.QueueBind("session-request", _exchangeName, "session-request");
@@ -40,12 +38,21 @@ namespace Server
             consumer.Received += (sender, args) =>
             {
                 var bodyJson = Encoding.UTF8.GetString(args.Body);
-                var responseMessage = JsonConvert.DeserializeObject<OpenSessionRequest>(bodyJson);
+                var requestMessage = JsonConvert.DeserializeObject<OpenSessionRequest>(bodyJson);
                 var responseToQueueName = (byte[])args.BasicProperties.Headers["response-queue"];
 
-                var response = new OpenSessionResponse { IsLogged = true };
-                var JsonResponse = JsonConvert.SerializeObject(response);
-                var body = Encoding.UTF8.GetBytes(JsonResponse);
+                var response = new OpenSessionResponse();
+                if (!_users.Contains(requestMessage.Login))
+                {
+                    response.IsLogged = true;
+                    _users.Add(requestMessage.Login);
+                }
+                else
+                {
+                    response.IsLogged = false;
+                }
+                var jsonResponse = JsonConvert.SerializeObject(response);
+                var body = Encoding.UTF8.GetBytes(jsonResponse);
                 channel.BasicPublish(_exchangeName, Encoding.UTF8.GetString(responseToQueueName), null, body);
             };
             channel.BasicConsume("session-request", true, consumer);
