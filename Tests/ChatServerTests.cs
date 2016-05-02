c0622441ad69c4e11dec4664157b37fa66dae242
rgawry <rgawry@users.noreply.github.com>
2016-05-02 14:46:11 +0200

    added ChatServerTests

---
+++ Tests/ChatServerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Chat
+{
+    [TestFixture]
+    public class ChatServerTests
+    {
+        [Test]
+        public void ShouldGivenUserLogIn()
+        {
+            const string userName = "login1";
+            var server = new ChitChatServer();
+            var request = new OpenSessionRequest { UserName = userName };
+
+            Assert.That(server.Handler(request).IsLogged, Is.True);
+            Assert.That(server.Handler(request).IsLogged, Is.False);
+        }
+    }
+}
