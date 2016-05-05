b518ca3eec4a89e1d8cd0bfae1c027107eaf10f6
rgawry <rgawry@users.noreply.github.com>
2016-05-05 12:39:53 +0200

    added one tmp queue for each client, removed one tmp queue for each client request

--- Chat.Messaging/RabbitMqClientBus.cs
+++ Chat.Messaging/RabbitMqClientBus.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using RabbitMQ.Client.Events;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,12 +8,22 @@ namespace Chat
 {
     public class RabbitMqClientBus : Bus, IClientBus
     {
-        public RabbitMqClientBus(string exchangeName, string requestQueueName) : base(exchangeName, requestQueueName) { }
+        private string _tmpQueue;
+
+        public RabbitMqClientBus(string exchangeName, string requestQueueName) : base(exchangeName, requestQueueName)
+        {
+        }
 
         public void Request<TRequest>(TRequest request)
         {
             var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(request));
             _channelProduce.BasicPublish(_exchangeName, _requestQueueName, null, body);
+        }
+
+        public new void Init()
+        {
+            base.Init();
+            _tmpQueue = _channelProduce.QueueDeclare().QueueName;
         }
 
         public Task<TResponse> Request<TRequest, TResponse>(TRequest request)
@@ -22,7 +33,7 @@ namespace Chat
             var consumerKey = GetConsumerKey();
             _channelsConsume.Add(consumerKey, _connection.CreateModel());
 
-            responseQueueName = _channelsConsume[consumerKey].QueueDeclare().QueueName;
+            responseQueueName = _tmpQueue; //_channelsConsume[consumerKey].QueueDeclare().QueueName;
             _channelsConsume[consumerKey].QueueBind(responseQueueName, _exchangeName, responseQueueName);
             var consumer = new EventingBasicConsumer(_channelsConsume[consumerKey]);
             consumer.Received += (sender, args) =>
