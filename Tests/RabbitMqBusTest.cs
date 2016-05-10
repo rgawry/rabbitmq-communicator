0421c4ed110647f069f0044420b8d5b9436f321a
rgawry <rgawry@users.noreply.github.com>
2016-05-10 10:06:20 +0200

    removed test from bus tests

--- Tests/RabbitMqBusTest.cs
+++ Tests/RabbitMqBusTest.cs
@@ -97,51 +97,5 @@ namespace Chat
             };
             Assert.That(testDelegate, Throws.TypeOf<TimeoutException>());
         }
-
-        [Test]
-        [Timeout(6000)]
-        public async Task ShouldOnlySecondRequestFromThreeTimeout()
-        {
-            var messageSerializer = new JsonMessageSerializer();
-            var exchangeName = "session-exchange";
-            var queueName = "session-request";
-            var connectionClient = CreateConnection();
-            var connectionServer = CreateConnection();
-
-            using (var clientBus = new RabbitMqClientBus(exchangeName, queueName, connectionClient, messageSerializer))
-            using (var serverBus = new RabbitMqServerBus(exchangeName, queueName, connectionServer, messageSerializer))
-            {
-                clientBus.Initialize();
-                serverBus.Initialize();
-
-                clientBus.TimeoutValue = 0.1f;
-
-                var request1 = new OpenSessionRequest { UserName = "login1" };
-                var request2 = new OpenSessionRequest { UserName = "login2" };
-                var request3 = new OpenSessionRequest { UserName = "login3" };
-
-                serverBus.AddHandler<OpenSessionRequest, OpenSessionResponse>(req =>
-                {
-                    if (req.UserName == "login1" || req.UserName == "login3") Thread.Sleep(1000);
-                    return new OpenSessionResponse { IsLogged = true };
-                });
-
-                AsyncTestDelegate testDelegate1 = async () =>
-                {
-                    var response1 = await clientBus.Request<OpenSessionRequest, OpenSessionResponse>(request1);
-                };
-
-                var response2 = await clientBus.Request<OpenSessionRequest, OpenSessionResponse>(request2);
-
-                AsyncTestDelegate testDelegate3 = async () =>
-                {
-                    var response3 = await clientBus.Request<OpenSessionRequest, OpenSessionResponse>(request3);
-                };
-
-                Assert.That(testDelegate1, Throws.TypeOf<TimeoutException>());
-                Assert.That(response2.IsLogged, Is.True);
-                Assert.That(testDelegate3, Throws.TypeOf<TimeoutException>());
-            };
-        }
     }
 }
