d2faf3329f545b147ef91218a948725ad78faefa
rgawry <rgawry@users.noreply.github.com>
2016-05-12 11:34:58 +0200

    added initial Display class

---
+++ Chat.Client/Display.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    public class Display : IDisplay
+    {
+        public void OnKeyboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
