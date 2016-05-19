25b17cd04eaffb06c6c89d7462ea6191a7af8163
rgawry <rgawry@users.noreply.github.com>
2016-05-19 10:18:48 +0200

    refactored CommandProcessor, added CommandProcessor tests

--- Chat/CommandProcessor.cs
+++ Chat/CommandProcessor.cs
@@ -4,10 +4,9 @@ namespace Chat
 {
     public class CommandProcessor : ICommandProcessor
     {
-
         public Tuple<string, string> Process(string value)
         {
-            var elements = value.Split(' ');
+            var elements = value.Split(new char[] { ' ' }, 2);
             if (elements.Length < 2)
             {
                 return new Tuple<string, string>(elements[0], string.Empty);
---
+++ Tests/CommandProcessorTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Chat
+{
+    [TestFixture]
+    public class CommandProcessorTest
+    {
+        [Test]
+        public void ShouldExtractCommandAndArgument()
+        {
+            var expectedCommand = "command";
+            var expectedArgument = "argument";
+            var line = expectedCommand + " " + expectedArgument;
+            var commandProcessor = new CommandProcessor();
+
+            var result = commandProcessor.Process(line);
+
+            Assert.That(result.Item1, Is.EqualTo(expectedCommand));
+            Assert.That(result.Item2, Is.EqualTo(expectedArgument));
+        }
+
+        [Test]
+        public void ShouldExtractArgumentsAsOneString()
+        {
+            var expectedCommand = "command";
+            var expectedArgument = "argument1 argument2";
+            var line = expectedCommand + " " + expectedArgument;
+            var commandProcessor = new CommandProcessor();
+
+            var result = commandProcessor.Process(line);
+
+            Assert.That(result.Item1, Is.EqualTo(expectedCommand));
+            Assert.That(result.Item2, Is.EqualTo(expectedArgument));
+        }
+
+        [Test]
+        public void ShouldExtractCommandOnly()
+        {
+            var expectedCommand = "command";
+            var line = expectedCommand;
+            var commandProcessor = new CommandProcessor();
+
+            var result = commandProcessor.Process(line);
+
+            Assert.That(result.Item1, Is.EqualTo(expectedCommand));
+            Assert.That(result.Item2, Is.EqualTo(string.Empty));
+        }
+    }
+}
