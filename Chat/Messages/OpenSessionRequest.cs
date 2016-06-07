3b890b7eb148e086dd90858d8fd34ec3d4e42f25
rgawry <rgawry@users.noreply.github.com>
2016-06-07 16:57:22 +0200

    removed internal from Token property set method

--- Chat/Messages/OpenSessionRequest.cs
+++ Chat/Messages/OpenSessionRequest.cs
@@ -2,7 +2,7 @@
 {
     public class OpenSessionRequest
     {
-        public string Token { get; internal set; }
+        public string Token { get; set; }
         public string UserName { get; set; }
     }
 }
--- Tests/JsonSerializerTest.cs
+++ Tests/JsonSerializerTest.cs
@@ -24,5 +24,19 @@ namespace Chat
             Assert.AreEqual(toSerialize.Item1, deserialized1.Item1);
             Assert.AreEqual(toSerialize.Item1, deserialized2.Item1);
         }
+
+        [Test]
+        public void SerializeDeserializeGuid2()
+        {
+            var toSerialize = new OpenSessionRequest { Token = Guid.NewGuid().ToString(), UserName = "testName" };
+            var jsonSerializer = new JsonMessageSerializer();
+
+            var serialized = jsonSerializer.Serialize(toSerialize);
+            var deserialized1 = jsonSerializer.Deserialize<OpenSessionRequest>(serialized);
+            var deserialized2 = jsonSerializer.Deserialize(serialized, typeof(OpenSessionRequest)) as OpenSessionRequest;
+
+            Assert.AreEqual(toSerialize.Token, deserialized1.Token);
+            Assert.AreEqual(toSerialize.Token, deserialized2.Token);
+        }
     }
 }
