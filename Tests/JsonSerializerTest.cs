3a47feccec3911724a8a8a232ac92f43dbc958d8
rgawry <rgawry@users.noreply.github.com>
2016-06-08 10:48:28 +0200

    removed test

--- Tests/JsonSerializerTest.cs
+++ Tests/JsonSerializerTest.cs
@@ -6,25 +6,6 @@ namespace Chat
     [TestFixture]
     class JsonSerializerTest
     {
-        class ClassToSerialize
-        {
-            public string Item1 { get; set; }
-        }
-
-        [Test]
-        public void SerializeDeserializeGuid()
-        {
-            var toSerialize = new ClassToSerialize { Item1 = Guid.NewGuid().ToString() };
-            var jsonSerializer = new JsonMessageSerializer();
-
-            var serialized = jsonSerializer.Serialize(toSerialize);
-            var deserialized1 = jsonSerializer.Deserialize<ClassToSerialize>(serialized);
-            var deserialized2 = jsonSerializer.Deserialize(serialized, typeof(ClassToSerialize)) as ClassToSerialize;
-
-            Assert.AreEqual(toSerialize.Item1, deserialized1.Item1);
-            Assert.AreEqual(toSerialize.Item1, deserialized2.Item1);
-        }
-
         [Test]
         public void SerializeDeserializeGuid2()
         {
